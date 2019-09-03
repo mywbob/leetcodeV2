488410fbec0a986101f9d84acb61acddf5b65eca
unknown <mywbob@gmail.com>
2019-09-03 16:14:22 +0800

    [Maximum Depth of Binary Tree] solution

---
+++ leetcode/Maximum Depth of Binary Tree.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/*
+ * time: O(n)
+ * space: O(n) worst case, O(logn) avg
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
